c6ee12b9b8586423112a7950e1fcf71d3c42dbbc
Dirk Mueller <dirkmuller8@outlook.de>
2023-06-22 14:36:54 +0200

    Added list of attendees, validation and removed a bug for viewing the attendees

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,38 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 
-namespace PartyInvites.Controllers;
-
-public class HomeController : Controller
+namespace PartyInvites.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [HttpGet]
-    public ViewResult RsvpForm()
-    {
-        return View();
-    }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
 
-    [HttpPost]
-    public ViewResult RsvpForm(GuestResponse guestResponse)
-    {
-        Repository.AddResponse(guestResponse);
-        return View("Thanks", guestResponse);
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
 }
